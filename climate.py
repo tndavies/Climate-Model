5a9409f9a071d9ffc89bdfcf084d28a73fdb16a2
Thomas Davies <thomasdavies1001@gmail.com>
2024-01-10 18:09:13 +0000

    Added back Antarctica altitude correction
    - When tidying the codebase, I forgot to include the altitude correction for Antarctica; this is now included again.

--- climate.py
+++ climate.py
@@ -10,31 +10,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # ---------------------------------------------------------------- #
+# 						Utility Functions 							
+# ---------------------------------------------------------------- #
+def days_to_seconds(x):		return x * 86400
+def days_to_years(x): 		return x / 365
+def years_to_days(x): 		return x * 365
+def seconds_to_years(x): 	return x / (86400 * 365)
+def to_kelvin(x): 			return x + 273.15
+def to_celsius(x): 			return x - 273.15
+def years_to_seconds(x):	return x * 365 * 86400
+
+def Is_AntarcticLatitudeBand(lat: float):
+	return lat >= -np.radians(90) and lat <= -np.radians(70)
+
+def Correct_SeaLevelTemperature(T: float, altitude_gain: float):
+	# @tidy: do we need to convert to celsius first here?
+	Lapse_Rate = 6.5 # degrees celsius lost, per 1km above sea-level.
+	Corrected_Temperature = to_celsius(T) - Lapse_Rate * (altitude_gain / 1000)
+	return to_kelvin(Corrected_Temperature)
+
+# ---------------------------------------------------------------- #
 # 						Simulation Parameters 							
 # ---------------------------------------------------------------- #
 
-Latitude_Step = 10 	# [angular degrees]
-Time_Step = 1 		# [days]
+Latitude_Step = 10 	# [degrees]
+Time_Step = days_to_seconds(1)
 
+Starting_Year = 1961
+Antarctica_Altitude = 2500			# [m]
 Orbital_Obliquity = -0.40911 		# [rads]
 Orbital_Period = 3.1558e7 			# [s]
 Orbital_SemiMajorAxis = 149.6e9 	# [m]
 Orbital_Eccentricity = 0.01671
-
 Sun_Luminosity = 3.846e26 			# [W]
-
-C_Transitioning_Ice = 5.31e7 	# [J/K]
-C_Land = 1.11e7 				# [J/K]
-C_Ocean = 2.201e8 				# [J/K]
-Diffusivity = 0.5394			# [???]
-
-# ---------------------------------------------------------------- #
-# 						Utility Functions 							
-# ---------------------------------------------------------------- #
-def days_to_seconds(days):	return days * 86400
-def days_to_years(days): 	return days / 365
-def years_to_days(days): 	return days * 365
-def to_kelvin(celsius): 	return celsius + 273.15
+C_Transitioning_Ice = 5.31e7 		# [J/K]
+C_Land = 1.11e7 					# [J/K]
+C_Ocean = 2.201e8 					# [J/K]
+Diffusivity = 0.5394				# [???]
 
 # ---------------------------------------------------------------- #
 # 					Diurnal Solar Radiation (S) 							
@@ -121,16 +133,27 @@ def calculate_Albedo(T: float):
 # ---------------------------------------------------------------- #
 # 						IR Cooling Function (I) 							
 # ---------------------------------------------------------------- #
-def calculate_IRCooling(T: float):
-	Band_Emission = 5.67e-8 * (T**4)
-	Atmospheric_Retension = ((T / 273)**3) * 0.635
-
-	return Band_Emission / (1 + Atmospheric_Retension)
-
-def calculate_IRCooling_Basic(T: float):
+def calculate_IRCooling(T, p):
 	SIGMA = 5.670374419e-8
 	TauIR = 0.79 * np.power((T / 273), 3)
 	return (SIGMA * np.power(T, 4)) / (1 + 0.75 * TauIR)
+
+def calculate_IRCooling2(T: float, t: float):
+	Band_Emission = 5.67e-8 * (T**4)
+
+	# Default CO2 concentration to the last historical value.
+	LastDataYear = list(Climate_CO2_Concentrations)[-1]
+	CO2_Concentration = Climate_CO2_Concentrations[LastDataYear]
+
+	# If a CO2 concentration exists in the historical record, for
+	# the period that we're in, then use this instead.
+	Current_Period = int(Starting_Year + seconds_to_years(t))
+	if Current_Period in Climate_CO2_Concentrations:
+		CO2_Concentration = Climate_CO2_Concentrations[Current_Period]
+
+	Atmospheric_Retension = ((T / 273)**3) * (CO2_Concentration/Climate_CO2_Concentrations[LastDataYear])**1.5
+
+	return Band_Emission / (1 + Atmospheric_Retension)
 
 # ---------------------------------------------------------------- #
 # 						Heat Capacity (C) 							
@@ -139,8 +162,8 @@ def calc_IceFraction(T: float):
 	# Computes the fraction of the land/ocean mass,
 	# within a latitude band, that is covered in snow/ice.
 	# (Vladilo, A.6)
-
-	return max(0, 1.0 - np.exp((T-273)/10))
+	param = (T - 273.0) / 10.0
+	return max(0, 1 - np.exp(param))
 
 def get_OceanFraction(lat: float):
 	# Performs a lookup into Earth's geography table to 
@@ -210,24 +233,22 @@ def calc_Temp_tROC(lats: list, temps: list, band_index: int, t: float):
 	Temp_sROC1 = calc_Temp_sROC1(lats, temps, band_index)
 	Temp_sROC2 = calc_Temp_sROC2(lats, temps, band_index)
 
-	IR_Cooling = calculate_IRCooling(Band_Temp)
 	Albedo = calculate_Albedo(Band_Temp)
 	Radiation_In = calc_AverageRadiation(lat, t)
 	Heat_Capacity = calc_AverageHeatCapacity(lat, Band_Temp)
+	IR_Cooling = calculate_IRCooling(Band_Temp, t)
 
 	Temp_tROC = (Radiation_In * (1 - Albedo) + Diffusivity * \
 		(Temp_sROC2 - np.tan(lat) * Temp_sROC1) - IR_Cooling) / Heat_Capacity
 
 	return Temp_tROC
 
-def SimClimate(starting_year: int, sim_duration: float):
+def SimClimate(sim_duration: float):
 	# Simulates the climate, starting at the specified year,
 	# for the specified number of years given by 'sim_duration';
 	# returns each time-point, relative to starting years, along
 	# with the temperature profile for each of these times.
 	# ---------------------------------------------------
-
-	Duration = years_to_days(sim_duration)
 
 	Lat_Grid = []
 	for Lat_Coord in np.arange(-90, 90 + Latitude_Step, Latitude_Step):
@@ -235,32 +256,36 @@ def SimClimate(starting_year: int, sim_duration: float):
 
 	# Retrieve the historical global average temperature for
 	# the specified starting year of the simulation.
-	assert(starting_year in Climate_Temperatures)
-	Starting_Temperature = Climate_Temperatures[starting_year]
+	assert(Starting_Year in Climate_Temperatures)
+	Starting_Temperature = Climate_Temperatures[Starting_Year]
 	Starting_Temperature = to_kelvin(Starting_Temperature)
 
 	Temperature_Sets = [[Starting_Temperature] * len(Lat_Grid)]
 	Time_Points = [0] # [Days]
 
 	# Metrics for the progress bar.
-	Total_Iterations = Duration / Time_Step
+	Total_Iterations = sim_duration / Time_Step
 	Num_Iterations_Done = 0
 
 	with alive_bar(title="Climate Simulation", manual=True) as bar:
 		# ---------------------------------------------------- 
-		while(Time_Points[-1] < Duration):
-			New_TemperatureProfile = []
-			
+		while(Time_Points[-1] < sim_duration):
 			Curr_TemperatureProfile = Temperature_Sets[-1]
 			Time = Time_Points[-1]
 
 			# ----- 
+			New_TemperatureProfile = []
 			for k in range(len(Lat_Grid)):
-				Temp_tROC = calc_Temp_tROC(Lat_Grid, Curr_TemperatureProfile, 
-							k, days_to_seconds(Time))
+				Temp_tROC =	calc_Temp_tROC(Lat_Grid, Curr_TemperatureProfile, k, Time)
 
-				New_BandTemperature = Curr_TemperatureProfile[k] + Temp_tROC * days_to_seconds(Time_Step)
+				Band_Temp = Curr_TemperatureProfile[k]
+				New_BandTemperature = Band_Temp + Temp_tROC * Time_Step
 				assert(New_BandTemperature >= 0.0)
+
+				# Correct sea-level temperature for Antarctica, so we get the
+				# correct temperature at the surface of the continent. 
+				if Is_AntarcticLatitudeBand(Lat_Grid[k]):
+					Band_Temp = Correct_SeaLevelTemperature(Band_Temp, Antarctica_Altitude)
 
 				New_TemperatureProfile.append(New_BandTemperature)
 			# ----- 
@@ -278,20 +303,27 @@ def SimClimate(starting_year: int, sim_duration: float):
 		# Remap time points in simultation to real world time,
 		# which is relative to the starting year.
 		for k in range(len(Time_Points)):
-			Time_Points[k] = starting_year + days_to_years(Time_Points[k])
+			Time_Points[k] = Starting_Year + seconds_to_years(Time_Points[k])
 
 	return Time_Points, Temperature_Sets
 
 # ---------------------------------------------------------------- #
 # 							Main Code Path 							
 # ---------------------------------------------------------------- #
+times, temps_sets = SimClimate(years_to_seconds(45))
 
-times, temps_sets = SimClimate(1961, 60)
+# foos = []
+# for k in temps_sets:
+# 	foos.append(k[0])
+
+# plt.plot(times, foos)
+# plt.show()
+
 idxs = np.arange(0, len(temps_sets)-1, 365)
 foo, temps = [], []
 for k in idxs:
 	foo.append(times[k])
-	temps.append(np.mean(temps_sets[k]))
+	temps.append(np.mean(temps_sets[k])) # this needs to be weighted based on band area!
 
 plt.figure()
 plt.xlabel("Years")
