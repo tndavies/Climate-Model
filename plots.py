57f38bb5c1199a6830ea8359b5055337f6fa82a8
Thomas Davies <thomasdavies1001@gmail.com>
2024-04-15 16:45:13 +0100

    backup

--- plots.py
+++ plots.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from climate import *
 import scienceplots
 import numpy as np
+from data import Wildfire_Damages
 
 def Get_ClimateRecordLength():
     return list(Historic_Temperatures)[-1] - list(Historic_Temperatures)[0]
@@ -72,7 +73,7 @@ def Fig_RCPs():
     Times, RCP26_Concentrations = Serialise(RCP26_Data)
     axis.plot(Times, RCP26_Concentrations, color="seagreen")
 
-    axis.fill_between(Times, RCP26_Concentrations, RCP85_Concentrations, color="black", alpha=0.1)
+    axis.fill_between(Times, RCP26_Concentrations, RCP85_Concentrations, color="slategrey", alpha=0.25)
 
     plt.show()
     
@@ -107,7 +108,7 @@ def Fig_AlbedoModel():
     
 def Fig_Irradiance():
     fig, (ax_Q, ax_irr, ax_dec) = plt.subplots(nrows=3,ncols=1,sharex=True)
-    fig.supxlabel("Year")
+    fig.supxlabel("Day")
 
     times = np.linspace(0, 86400*365, 1000)
     Plot_Times = np.divide(times, 86400)
@@ -166,24 +167,24 @@ def Fig_HeatCapacityTest():
     Ocean_Lat, Land_Lat = np.radians(-55.0), np.radians(-90.0)
     ocean_hc = [calc_AverageHeatCapacity(Ocean_Lat, T) for T in temps]
     land_hc = [calc_AverageHeatCapacity(Land_Lat, T) for T in temps]
-    hc_ices = [C_Transitioning_Ice if(T >= 263 and T <= 273) else C_Land for T in temps]
+    # hc_ices = [C_Transitioning_Ice if(T >= 263 and T <= 273) else C_Land for T in temps]
 
-    ax_hcaps.plot(temps, np.divide(ocean_hc,1e6), "-", color="steelblue")
-    ax_hcaps.plot(temps, np.divide(land_hc, 1e6), "-", color="peru")
-    ax_hcaps.plot(temps, np.divide(hc_ices, 1e6), "--", color="black", alpha=0.45, linewidth=0.5) 
-    ax_hcaps.set_ylabel(r"$\tilde{C}$ (MJ K$^{-1}$)")
+    ax_hcaps.plot(temps, np.divide(ocean_hc,1e6), "-", color="steelblue", linewidth=2.0)
+    ax_hcaps.plot(temps, np.divide(land_hc, 1e6), "-", color="peru", linewidth=2.0)
+    # ax_hcaps.plot(temps, np.divide(hc_ices, 1e6), "--", color="black", alpha=0.5) 
+    ax_hcaps.set_ylabel(r"$\tilde{C}$ (MJ K$^{-1}$)", fontsize=17)
 
     ice_fracs = [Calc_IceFraction(T) for T in temps]
     ax_ice.plot(temps, ice_fracs, "-", color="black")
-    ax_ice.set_ylabel(r"f$_i$")
+    ax_ice.set_ylabel(r"f$_i$", fontsize=18)
 
     plt.show()
     
 def Fig_Geography():
     fig, (ax) = plt.subplots(nrows=1,ncols=1,sharex=False)
 
-    ax.set_xlabel("Latitude")
-    ax.set_ylabel(r"f$_L$")
+    ax.set_xlabel(r"$\lambda$", fontsize=16)
+    ax.set_ylabel(r"f$_L$", fontsize=16)
     
     lats = np.linspace(-90, 90, 1000)
     fl = [1.0 - Get_OceanFraction(lat, to_degrees=False) for lat in lats]
@@ -229,7 +230,7 @@ def Fig_DipDependance():
     # climate in a constant atmospheric CO2 concentration, with the uniform
     # temperature IC, varying the CO2 to show how the dip responds.
 
-    ax.set_xlabel("Year")
+    ax.set_xlabel("Time (years)")
     ax.set_ylabel("Temperature (K)")
 
     Sim_Duration = 35
@@ -240,4 +241,88 @@ def Fig_DipDependance():
     
     plt.show()
 
-Fig_AntarcticaCorrection()
+def Fig_Forecasts():
+    Target_Year = 2100
+    Duration = 1 + (Target_Year - list(Historic_Temperatures)[0])
+   
+    Duration = 2
+    Sim_RCP85 = Simulate_Climate(Sim_Specification(Duration, RCP=RCP85, InitialTempDist=Equilibrium_Config))
+    # Sim_RCP45 = Simulate_Climate(Sim_Specification(Duration, RCP=RCP45, InitialTempDist=Equilibrium_Config))
+    # Sim_RCP6 = Simulate_Climate(Sim_Specification(Duration, RCP=RCP6, InitialTempDist=Equilibrium_Config))
+    # Sim_RCP26 = Simulate_Climate(Sim_Specification(Duration, RCP=RCP26, InitialTempDist=Equilibrium_Config))
+    Sim_Context = Sim_RCP85
+ 
+	# GAT Forecast plot
+  	# ----------------------------------------------------------------
+    # fig, (ax) = plt.subplots(nrows=1,ncols=1,sharex=False)
+    # ax.set_xlabel("Time (years)")
+    # ax.set_ylabel("Temperature (K)")
+
+    # LastYearOnRecord = (list(Historic_Temperatures)[-1] - Sim_Context.spec.Initial_Year)*31536000
+    # N = round(LastYearOnRecord / Sim_Context.spec.Time_Step)
+    # N = 0
+
+    # Times = np.array(Sim_RCP85.times)[N::365]
+    # GATs_RCP85 = np.array(Average(Sim_RCP85, lambda x: x, (-90,90)))[N::365]
+    # ax.plot(Times, GATs_RCP85, "-", color="firebrick")
+
+    # Times = np.array(Sim_RCP6.times)[N::365]
+    # GATs = np.array(Average(Sim_RCP6, lambda x: x, (-90,90)))[N::365]
+    # ax.plot(Times, GATs, "-", color="darkorange")
+
+    # Times = np.array(Sim_RCP45.times)[N::365]
+    # GATs = np.array(Average(Sim_RCP45, lambda x: x, (-90,90)))[N::365]
+    # ax.plot(Times, GATs, "-", color="royalblue")
+    
+    # Times = np.array(Sim_RCP26.times)[N::365]
+    # GATs_RCP26 = np.array(Average(Sim_RCP26, lambda x: x, (-90,90)))[N::365]
+    # ax.plot(Times, GATs_RCP26, "-", color="seagreen")
+
+    # ax.fill_between(Times, GATs_RCP26, GATs_RCP85, color="slategrey", alpha=0.25)
+
+    # plt.show()
+    
+    # Temperature Distributions in the year 2100
+  	# ----------------------------------------------------------------
+    fig, (ax) = plt.subplots(nrows=1,ncols=1,sharex=False)
+    ax.set_xlabel(r"$\lambda$")
+    ax.set_ylabel("Temperature (K)")
+
+    avg_td = []
+    tdists_2100 = np.array(Sim_RCP85.tps)[-365:]
+    for idx in range(len(Sim_Context.spec.lats)):
+        td_zonal_temps = [td[idx] for td in tdists_2100]
+        avg_td.append(np.mean(td_zonal_temps))
+
+    ax.plot(np.degrees(Sim_Context.lats), avg_td, "r-") 
+
+    plt.show()
+     
+	# Plot global temperature distributions
+	# fig, (axis) = plt.subplots(nrows=1,ncols=1,sharex=False,figsize=(6.4, 4))
+  
+	# def plot_dist(sim: Sim_Result, name: str, col: str, line_pattern: str = "-"):
+	# 	Average_Temps = []
+
+	# 	Steps_Per_Year = int(3.154e7 / sim.spec.Time_Step) + 1
+	# 	Temp_Dists = [sim.tps[-k] for k in range(1,Steps_Per_Year + 1)]
+  
+	# 	for k in range(len(sim.lats)):
+	# 		buffer = [arr[k] for arr in Temp_Dists]
+	# 		Average_Temps.append(np.mean(buffer))
+  
+	# 	axis.plot(np.degrees(sim.lats), Average_Temps, label=name, color=col, linestyle=line_pattern, linewidth=0.8)
+
+	# plot_dist(Sim_Context, "2023", Context_Col, line_pattern="--")
+	# plot_dist(Sim_RCP85, "RCP 8.5", RCP85_Col)
+	# # plot_dist(Sim_RCP6, "RCP 6", RCP6_Col)
+	# plot_dist(Sim_RCP45, "RCP 4.5", RCP45_Col)
+	# plot_dist(Sim_RCP26, "RCP 2.6", RCP26_Col)
+
+	# axis.set_xlabel("Latitude")
+	# axis.set_ylabel("Temperature (k)")
+	# axis.legend()
+
+	# Save_Figure(fig, "tdist_forecast")
+ 
+Fig_Forecasts()
